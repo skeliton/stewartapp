455301f3ea66fdda489372b366b3f075eaf633a5
Dan Stewart <dannstewart@outlook.com>
2021-02-17 14:02:38 -0600

    adding nav and shared components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,14 @@ import { UsefultoolingComponent } from './usefultooling/usefultooling.component'
 import { PathNotFoundComponent } from './path-not-found/path-not-found.component';
 import { AdminModule } from './admin/admin.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { SharedComponent } from './shared/shared.component';
 
 @NgModule({
   declarations: [
@@ -16,13 +24,21 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     DashboardComponent,
     ExperimentalComponent,
     UsefultoolingComponent,
-    PathNotFoundComponent
+    PathNotFoundComponent,
+    NavComponent,
+    SharedComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AdminModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
